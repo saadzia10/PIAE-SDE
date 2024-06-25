09a4e43d52bf5143f8aa4bd043adadc6ba489ce3
Saad Zia <msaadzia7@gmail.com>
2024-06-25 14:58:36 +0500

    Initial version for climate model

---
+++ physvae/climate/model.py
@@ -0,0 +1,168 @@
+
+import sys; sys.path.append('../')
+from mlp import MLP
+import torch
+import torch.nn as nn
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, config):
+        super(FeatureExtractor, self).__init__()
+        layers = []
+        input_dim = config['dim_t']
+        for output_dim in config['hidlayers_feat']:
+            layers.append(nn.Linear(input_dim, output_dim))
+            layers.append(nn.ReLU())
+            input_dim = output_dim
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class Encoders(nn.Module):
+    def __init__(self, config: dict):
+        super(Encoders, self).__init__()
+
+        self.dim_t = config['dim_t']
+        self.dim_z_phy = config['dim_z_phy']  # 1 for E_0
+        self.dim_z_aux = config['dim_z_aux']  # 1 for rb_n
+        self.activation = config['activation']
+
+        # Feature extractor
+        self.func_feat = FeatureExtractor(config)
+
+        # Encoders for z_phy (E_0)
+        # Unmixing network
+        self.func_unmixer = MLP(
+            config['hidlayers_unmixer'] + [self.dim_t, ], self.activation)
+
+        self.func_z_phy_mean = MLP([self.dim_t, ] + config['hidlayers_phy'] + [self.dim_z_phy, ], self.activation)
+        self.func_z_phy_lnvar = MLP([self.dim_t, ] + config['hidlayers_phy'] + [self.dim_z_phy, ], self.activation)
+
+        # Encoders for z_aux (rb_n)
+        if self.dim_z_aux > 0:
+            self.func_z_aux_mean = MLP([self.dim_t, ] + config['hidlayers_aux_enc'] + [self.dim_z_aux, ],
+                                       self.activation)
+            self.func_z_aux_lnvar = MLP([self.dim_t, ] + config['hidlayers_aux_enc'] + [self.dim_z_aux, ],
+                                        self.activation)
+
+    def forward(self, x):
+        features = self.func_feat(x)
+        unmixed = self.func_unmixer(features)
+
+        z_phy_mean = self.func_z_phy_mean(unmixed)
+        z_phy_lnvar = self.func_z_phy_lnvar(unmixed)
+
+        if self.dim_z_aux > 0:
+            z_aux_mean = self.func_z_aux_mean(unmixed)
+            z_aux_lnvar = self.func_z_aux_lnvar(unmixed)
+        else:
+            z_aux_mean = torch.zeros(features.size(0), 0, device=features.device)
+            z_aux_lnvar = torch.zeros(features.size(0), 0, device=features.device)
+
+        return z_phy_mean, z_phy_lnvar, z_aux_mean, z_aux_lnvar, unmixed
+
+
+class Decoders(nn.Module):
+    def __init__(self, config: dict):
+        super(Decoders, self).__init__()
+
+        self.T_ref = 15.0
+        self.T_0 = -46.02
+        self.dim_z_phy = config['dim_z_phy']  # Assuming dim_z_phy = 1 for E_0
+        self.dim_z_aux = config['dim_z_aux']  # Assuming dim_z_aux = 1 for rb_n
+        self.dim_t = config['dim_t']
+        self.activation = config['activation']
+
+        # Physics function for E_0
+        self.func_phy = nn.Linear(self.dim_z_phy, 1)
+
+        # Auxiliary function for rb_n
+        if self.dim_z_aux > 0:
+            self.func_aux = MLP([self.dim_z_aux + 1, ] + config['hidlayers_aux_dec'] + [1, ],
+                                self.activation)
+
+        # Register buffer for log variance
+        self.register_buffer('param_x_lnvar', torch.ones(1) * config['x_lnvar'])
+
+    def forward(self, z_phy, z_aux, T_air):
+        # Physics-based model output
+        E_0 = z_phy[:, 0]
+        term = (1 / (self.T_ref - self.T_0) - 1 / (T_air - self.T_0))
+        NEE_phy = E_0 * term
+
+        # Auxiliary model output
+        if self.dim_z_aux > 0:
+            aux_input = torch.cat([z_aux, T_air.view(-1, 1)], dim=1)
+            rb_n = self.func_aux(aux_input)
+            NEE = rb_n * NEE_phy.unsqueeze(1)
+        else:
+            NEE = NEE_phy.unsqueeze(1)
+
+        return NEE, self.param_x_lnvar
+
+
+class ClimateVAE(nn.Module):
+    def __init__(self, config):
+        super(ClimateVAE, self).__init__()
+        self.enc = Encoders(config)
+        self.dec = Decoders(config)
+
+    def generate_phyonly(self, z_phy, T_air):
+        # Physics-based model output
+        E_0 = z_phy[:, 0]
+        term = (1 / (self.dec.T_ref - self.dec.T_0) - 1 / (T_air - self.dec.T_0))
+        NEE_phy = E_0 * term
+        return NEE_phy.unsqueeze(1)
+
+    def encode(self, x):
+        z_phy_mean, z_phy_lnvar, z_aux_mean, z_aux_lnvar, unmixed = self.enc(x)
+        z_phy_stat = {'mean': z_phy_mean, 'lnvar': z_phy_lnvar}
+        z_aux_stat = {'mean': z_aux_mean, 'lnvar': z_aux_lnvar}
+
+        return z_phy_stat, z_aux_stat, unmixed
+
+    def priors(self, n, device):
+        # Prior for E_0 (assumed to be normally distributed)
+        prior_z_phy_mean = torch.full((n, 1), 0.0, device=device)
+        prior_z_phy_lnvar = torch.full((n, 1), 0.0, device=device)  # ln(1) = 0 for unit variance
+
+        # Prior for rb_n (assumed to be normally distributed)
+        prior_z_aux_mean = torch.full((n, 1), 0.0, device=device)
+        prior_z_aux_lnvar = torch.full((n, 1), 0.0, device=device)  # ln(1) = 0 for unit variance
+
+        prior_z_phy_stat = {'mean': prior_z_phy_mean, 'lnvar': prior_z_phy_lnvar}
+        prior_z_aux_stat = {'mean': prior_z_aux_mean, 'lnvar': prior_z_aux_lnvar}
+
+        return prior_z_phy_stat, prior_z_aux_stat
+
+    def reparameterize(self, mean, lnvar):
+        std = torch.exp(0.5 * lnvar)
+        eps = torch.randn_like(std)
+        return mean + eps * std
+
+    def draw(self, z_phy_stat, z_aux_stat, hard_z=False):
+        z_phy_mean = z_phy_stat['mean']
+        z_phy_lnvar = z_phy_stat['lnvar']
+        z_aux_mean = z_aux_stat['mean']
+        z_aux_lnvar = z_aux_stat['lnvar']
+
+        if hard_z:
+            z_phy = z_phy_mean
+            z_aux = z_aux_mean
+        else:
+            z_phy = self.reparameterize(z_phy_mean, z_phy_lnvar)
+            z_aux = self.reparameterize(z_aux_mean, z_aux_lnvar)
+
+        return z_phy, z_aux
+
+    def decode(self, z_phy, z_aux, T_air):
+        return self.dec(z_phy, z_aux, T_air)
+
+    def forward(self, x, T_air):
+        z_phy_stat, z_aux_stat, unmixed = self.encode(x)
+        z_phy = self.reparameterize(z_phy_stat['mean'], z_phy_stat['lnvar'])
+        z_aux = self.reparameterize(z_aux_stat['mean'], z_aux_stat['lnvar'])
+        NEE, lnvar = self.decode(z_phy, z_aux, T_air)
+        return z_phy_stat, z_aux_stat, NEE, lnvar
---
+++ physvae/climate/train.py
@@ -0,0 +1,285 @@
+import argparse
+import os.path
+
+import pandas as pd
+import json
+import time
+import numpy as np
+
+import torch
+from torch import optim
+import torch.utils.data
+
+from model import ClimateVAE  # Adjusted import for the ClimateVAE model
+import utils
+
+def set_parser():
+    parser = argparse.ArgumentParser(description='')
+
+    # input/output setting
+    parser.add_argument('--outdir', type=str, required=True)
+    parser.add_argument('--datadir', type=str, required=True)
+    parser.add_argument('--dataname-train', type=str, default='train')
+    parser.add_argument('--dataname-valid', type=str, default='valid')
+
+    # prior knowledge
+    parser.add_argument('--range-omega', type=float, nargs=2, default=[0.0, np.pi])
+
+    # model (general)
+    parser.add_argument('--dim-z-phy', type=int, default=1)
+    parser.add_argument('--dim-z-aux', type=int, default=1)
+    parser.add_argument('--activation', type=str, default='elu') #choices=['relu','leakyrelu','elu','softplus','prelu'],
+    parser.add_argument('--no-phy', action='store_true', default=False)
+
+    # model (decoder)
+    parser.add_argument('--x-lnvar', type=float, default=-8.0)
+    parser.add_argument('--hidlayers-aux-dec', type=int, nargs='+', default=[128,])
+
+    # model (encoder)
+    parser.add_argument('--hidlayers-aux-enc', type=int, nargs='+', default=[128,])
+    parser.add_argument('--hidlayers-unmixer', type=int, nargs='+', default=[128,])
+    parser.add_argument('--hidlayers-phy', type=int, nargs='+', default=[128])
+    parser.add_argument('--arch-feat', type=str, default='mlp')
+    parser.add_argument('--num-units-feat', type=int, default=256)
+    parser.add_argument('--hidlayers-feat', type=int, nargs='+', default=[128,])
+    parser.add_argument('--num-rnns-feat', type=int, default=1)
+
+    # optimization (base)
+    parser.add_argument('--learning-rate', type=float, default=1e-3)
+    parser.add_argument('--weight-decay', type=float, default=1e-3)
+    parser.add_argument('--adam-eps', type=float, default=1e-3)
+    parser.add_argument('--grad-clip', type=float, default=10.0)
+    parser.add_argument('--batch-size', type=int, default=200)
+    parser.add_argument('--epochs', type=int, default=5000)
+    parser.add_argument('--balance-kld', type=float, default=1.0)
+    parser.add_argument('--balance-unmix', type=float, default=0.0)
+    parser.add_argument('--balance-dataug', type=float, default=0.0)
+    parser.add_argument('--balance-lact-dec', type=float, default=0.0)
+    parser.add_argument('--balance-lact-enc', type=float, default=0.0)
+
+    # others
+    parser.add_argument('--train-size', type=int, default=-1)
+    parser.add_argument('--save-interval', type=int, default=999999999)
+    parser.add_argument('--num-workers', type=int, default=0)
+    parser.add_argument('--cuda', action='store_true', default=False)
+    parser.add_argument('--seed', type=int, default=1234567890)
+
+    return parser
+
+
+def loss_function(args, data, z_phy_stat, z_aux_stat, x_mean):
+    n = data.shape[0]
+    device = data.device
+
+    # Reconstruction error (Mean Squared Error)
+    recerr_sq = torch.sum((x_mean - data).pow(2), dim=1).mean()
+
+    # Priors for the latent variables
+    prior_z_phy_stat, prior_z_aux_stat = model.priors(n, device)
+
+    # KL divergence for auxiliary latent variables (rb_n)
+    KL_z_aux = utils.kldiv_normal_normal(z_aux_stat['mean'], z_aux_stat['lnvar'],
+                                         prior_z_aux_stat['mean'],
+                                         prior_z_aux_stat['lnvar']) if args.dim_z_aux > 0 else torch.zeros(1,
+                                                                                                           device=device)
+
+    # KL divergence for physics latent variables (E_0)
+    KL_z_phy = utils.kldiv_normal_normal(z_phy_stat['mean'], z_phy_stat['lnvar'],
+                                         prior_z_phy_stat['mean'],
+                                         prior_z_phy_stat['lnvar']) if not args.no_phy else torch.zeros(1,
+                                                                                                        device=device)
+
+    # Total KL divergence
+    kldiv = (KL_z_aux + KL_z_phy).mean()
+
+    return recerr_sq, kldiv
+
+
+def train(epoch, args, device, loader, model, optimizer):
+    model.train()
+    logs = {'recerr_sq':.0, 'kldiv':.0, 'unmix':.0, 'dataug':.0, 'lact_dec':.0}
+
+    for batch_idx, (data, T_air, NEE) in enumerate(loader):
+        data = data.to(device)
+        T_air = T_air.to(device)
+        batch_size = len(data)
+        optimizer.zero_grad()
+
+        # inference & reconstruction on original data
+        z_phy_stat, z_aux_stat, unmixed = model.encode(data)
+        z_phy, z_aux = model.draw(z_phy_stat, z_aux_stat, hard_z=False)
+        NEE_pred, lnvar = model.decode(z_phy, z_aux, T_air)
+
+        # ELBO
+        recerr_sq, kldiv = loss_function(args, NEE, z_phy_stat, z_aux_stat, NEE_pred)
+
+        # unmixing regularization (R_{DA,1})
+        reg_unmix = torch.sum((unmixed - z_phy.detach()).pow(2), dim=1).mean()
+
+        # least action principle (R_ppc)
+        # least action principle (R_ppc)
+        # Calculate differences between the reconstructions
+        NEE_PA, _ = model.decode(z_phy, torch.zeros_like(z_aux), T_air)
+        NEE_PB, _ = model.decode(torch.zeros_like(z_phy), z_aux, T_air)
+
+        dif_PA_P = torch.sum((NEE_PA - NEE).pow(2), dim=1).mean()
+        dif_PB_P = torch.sum((NEE_PB - NEE).pow(2), dim=1).mean()
+        dif_PAB_PA = torch.sum((NEE - NEE_PA).pow(2), dim=1).mean()
+        dif_PAB_PB = torch.sum((NEE - NEE_PB).pow(2), dim=1).mean()
+        reg_lact_dec = 0.25 * dif_PA_P + 0.25 * dif_PB_P + 0.25 * dif_PAB_PA + 0.25 * dif_PAB_PB
+
+        # loss function
+        kldiv_balanced = (args.balance_kld + args.balance_lact_enc) * kldiv
+        loss = recerr_sq + kldiv_balanced \
+            + args.balance_unmix * reg_unmix + args.balance_lact_dec * reg_lact_dec
+
+        # update model parameters
+        loss.backward()
+        if args.grad_clip > 0.0:
+            torch.nn.utils.clip_grad_value_(model.parameters(), args.grad_clip)
+        optimizer.step()
+
+        # log
+        logs['recerr_sq'] += recerr_sq.detach() * batch_size
+        logs['kldiv'] += kldiv.detach() * batch_size
+        logs['unmix'] += reg_unmix.detach() * batch_size
+        # logs['dataug'] += reg_dataug.detach() * batch_size
+        logs['lact_dec'] += reg_lact_dec.detach() * batch_size
+
+    for key in logs:
+        logs[key] /= len(loader.dataset)
+    print('====> Epoch: {}  Training (rec. err.)^2: {:.4f}  kldiv: {:.4f}  unmix: {:.4f}  dataug: {:.4f}  lact_dec: {:.4f}'.format(
+        epoch, logs['recerr_sq'], logs['kldiv'], logs['unmix'], logs['dataug'], logs['lact_dec']))
+    return logs
+
+
+def valid(epoch, args, device, loader, model):
+    model.eval()
+    logs = {'recerr_sq': .0, 'kldiv': .0}
+    with torch.no_grad():
+        for i, (data, T_air, NEE) in enumerate(loader):
+            data = data.to(device)
+            T_air = T_air.to(device)
+            batch_size = len(data)
+
+            # inference & reconstruction on original data
+            z_phy_stat, z_aux_stat, unmixed = model.encode(data)
+            z_phy, z_aux = model.draw(z_phy_stat, z_aux_stat, hard_z=False)
+            NEE_pred, lnvar = model.decode(z_phy, z_aux, T_air)
+
+            # ELBO
+            recerr_sq, kldiv = loss_function(args, NEE, z_phy_stat, z_aux_stat, NEE_pred)
+
+            # log
+            logs['recerr_sq'] += recerr_sq.detach() * batch_size
+            logs['kldiv'] += kldiv.detach() * batch_size
+
+    for key in logs:
+        logs[key] /= len(loader.dataset)
+    print('====> Epoch: {}  Validation (rec. err.)^2: {:.4f}  kldiv: {:.4f}'.format(
+        epoch, logs['recerr_sq'], logs['kldiv']))
+    return logs
+
+
+def create_data_loader(file_path, batch_size=32, shuffle=True, get_dim=False, kwargs=None):
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+
+    # Convert data to tensors
+    data_tensor = torch.tensor(df.values, dtype=torch.float32)
+    ta_col = torch.tensor(df['Ta'].values, dtype=torch.float32)
+    nee_col = torch.tensor(df['NEE'].values, dtype=torch.float32)
+
+    # Create TensorDataset
+    dataset = torch.utils.data.TensorDataset(data_tensor, ta_col, nee_col)
+
+    # Create DataLoader
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                                              **kwargs) if kwargs else torch.utils.data.DataLoader(dataset,
+                                                                                                   batch_size=batch_size,
+                                                                                                   shuffle=shuffle)
+    if not get_dim:
+        return data_loader
+    return data_loader, df.shape[1]
+
+
+if __name__ == '__main__':
+
+    parser = set_parser()
+    args = parser.parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+
+    # set random seed
+    torch.manual_seed(args.seed)
+
+
+    # load training/validation data
+    train_file_path = args.datadir + '/train_night_subset_train.csv'
+    val_file_path = args.datadir + '/train_night_subset_train.csv'
+
+
+    # set data loaders
+    kwargs = {'num_workers': args.num_workers, 'pin_memory': True} if args.cuda else {}
+
+    # Create DataLoaders
+    loader_train, dims = create_data_loader(train_file_path, batch_size=args.batch_size, shuffle=True, get_dim=True, kwargs= kwargs)
+    loader_valid = create_data_loader(val_file_path, batch_size=args.batch_size, shuffle=False, kwargs= kwargs)
+
+    args.dim_t = dims
+    # set model
+    model = ClimateVAE(vars(args)).to(device)
+
+    # set optimizer
+    kwargs = {'lr': args.learning_rate, 'weight_decay': args.weight_decay, 'eps': args.adam_eps}
+    optimizer = optim.Adam(model.parameters(), **kwargs)
+
+    print('start training with device', device)
+    print(vars(args))
+    print()
+
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+    # save args
+    with open('{}/args.json'.format(args.outdir), 'w') as f:
+        json.dump(vars(args), f, sort_keys=True, indent=4)
+
+    # create log files
+    with open('{}/log.txt'.format(args.outdir), 'w') as f:
+        print('# epoch recerr_sq kldiv unmix dataug lact_dec valid_recerr_sq valid_kldiv duration', file=f)
+
+
+    # main iteration
+    info = {'bestvalid_epoch':0, 'bestvalid_recerr':1e10}
+    dur_total = .0
+    for epoch in range(1, args.epochs + 1):
+        # training
+        start_time = time.time()
+        logs_train = train(epoch, args, device, loader_train, model, optimizer)
+        dur_total += time.time() - start_time
+
+        # validation
+        logs_valid = valid(epoch, args, device, loader_valid, model)
+
+        # save loss information
+        with open('{}/log.txt'.format(args.outdir), 'a') as f:
+            print('{} {:.7e} {:.7e} {:.7e} {:.7e} {:.7e} {:.7e} {:.7e} {:.7e}'.format(epoch,
+                logs_train['recerr_sq'], logs_train['kldiv'], logs_train['unmix'], logs_train['dataug'], logs_train['lact_dec'],
+                logs_valid['recerr_sq'], logs_valid['kldiv'], dur_total), file=f)
+
+        # save model if best validation loss is achieved
+        if logs_valid['recerr_sq'] < info['bestvalid_recerr']:
+            info['bestvalid_epoch'] = epoch
+            info['bestvalid_recerr'] = logs_valid['recerr_sq']
+            torch.save(model.state_dict(), '{}/model.pt'.format(args.outdir))
+            print('best model saved')
+
+        # save model at interval
+        if epoch % args.save_interval == 0:
+            torch.save(model.state_dict(), '{}/model_e{}.pt'.format(args.outdir, epoch))
+
+        print()
+
+    print()
+    print('end training')
